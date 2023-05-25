40e593d9f751e2a657804b5017eac80f5b2900b4
Elliot Vaucher <ervaucher@gmail.com>
2023-05-25 12:48:19 +0200

    Update index.js

--- src/pages/assistant/index.js
+++ src/pages/assistant/index.js
@@ -14,7 +14,7 @@ export const MailUs = () => {
     response: "", // Added field for OpenAI's response
   });
 
-  const API_KEY = process.env.API_KEY; // API Key for Chat-GPT API
+  const API_KEY = process.env.REACT_APP_API_KEY; // API Key for Chat-GPT API
 
   const handleSubmit = async (e) => {
     e.preventDefault();
